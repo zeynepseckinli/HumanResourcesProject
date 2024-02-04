fcf994e80eb462b55eaafc235cac6dacb616f819
zeynep <zeyneps1001@gmail.com>
2024-02-04 21:55:30 +0300

    updated

--- UserMicroService/src/main/java/com/bilgeadam/repository/entity/Company.java
+++ UserMicroService/src/main/java/com/bilgeadam/repository/entity/Company.java
@@ -7,14 +7,23 @@ import lombok.experimental.SuperBuilder;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.time.LocalDate;
+
 @SuperBuilder
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 @Document
-public class Company  extends BaseEntity{
+public class Company extends BaseEntity {
 
     @Id
     private String id;
     private String name;
+    private String taxNumber;
+    private String title;
+    private String address;
+    private String phone;
+    private String email;
+    private int numberOfEmployees;
+    private LocalDate yearOfEstablishment;
 }
